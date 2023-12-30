fccfdf90586bc8bdfba5673a4004bffae6d83d0b
George Rey <1606347+jerzydziewierz@users.noreply.github.com>
2023-12-30 18:28:09 +0000

    re-init

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import jax
+import flax
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    exit()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
